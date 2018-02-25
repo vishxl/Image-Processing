6b1caf0b255315d6ebfd853873d6fedfd36744d3
Vishal Singh <vsalsngh@gmail.com>
2018-02-25 13:47:30 +0530

    image size

--- Task1/task1.cpp
+++ Task1/task1.cpp
@@ -1,16 +1,42 @@
 #include<opencv2/opencv.hpp>
 #include<iostream>
+#include <stdlib.h>
 
 using namespace std;
 using namespace cv;
 
-int main() {
 
-	cv::Mat img = cv::imread("rmlo2011_0.jpg", 0);
-	for (int i = 0; i<img.rows; i++)
-		for (int j = 0; i<img.cols; j++)
-			std::cout << img.at<uchar>(i, j) << std::endl;
+int main() {
+	cv::Mat img = cv::imread("rmlo2011_0.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+	//cout << "img = " << endl << " " << img << endl << endl;
+
+	int rows = img.rows;
+	int cols = img.cols;
+
+	cv::Size s = img.size();
+	rows = s.height;
+	cols = s.width;
+
+	cout << rows << " " << cols << endl;
+
+	cout << rows << " " << cols << endl;
+	cout << s << endl;
+
+	cv::imshow("Before", img);
+
+	for (int j = 0; j<img.rows; j++)
+	{
+		for (int i = 0; i<img.cols; i++)
+		{
+			if (i == j)
+				img.at<uchar>(j, i) = 255; //white
+		}
+	}
+	//After changing
+	cv::imshow("After", img);
+
 	waitKey(0);
 	return 0;
-
 }
+
