f90ceb333c479587a97dc2be59e031a2d65ac6aa
Vishal Singh <vsalsngh@gmail.com>
2018-02-26 16:35:20 +0530

    histogram

--- Task1/task1.cpp
+++ Task1/task1.cpp
@@ -5,7 +5,23 @@
 using namespace std;
 using namespace cv;
 
+int histSize = 3;    // bin size
+float range[] = { 0, 255 };
+const float *ranges[] = { range };
 
+Mat computeHistogram(Mat input_image) {
+
+	Mat histogram;
+	int channels[] = { 0 };
+	int histSize[] = { 256 };
+	float range[] = { 0, 256 };
+	const float* ranges[] = { range };
+
+	calcHist(&input_image, 1, channels, Mat(), histogram, 1, histSize, ranges, true, false);
+
+	return histogram;
+
+} 
 int main() {
 	cv::Mat img = cv::imread("rmlo2011_0.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
@@ -23,6 +39,12 @@ int main() {
 	cout << rows << " " << cols << endl;
 	cout << s << endl;
 
+	Mat histogram = computeHistogram(img);
+
+	cout << "Histogram...\n";
+	for (int i = 0; i < histogram.rows; ++i)
+		cout << i << " : " << histogram.at<int>(i, 0) << "\n";
+
 	cv::imshow("Before", img);
 
 	for (int j = 0; j<img.rows; j++)
@@ -35,6 +57,8 @@ int main() {
 	}
 	//After changing
 	cv::imshow("After", img);
+	
+
 
 	waitKey(0);
 	return 0;
